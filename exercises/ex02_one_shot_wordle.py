42ecef30a197eae691f690f2f3e764ded3cf22ec
Christopher Clark <rchris@ad.unc.edu>
2023-02-13 16:16:06 -0500

    FINISHED CORRECT EX02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,44 +1,50 @@
-"""EX02 - one shot wordle"""
+"""EX02 - one shot wordle."""
 __author__ = "730606845"
-
 secret_word: str = "python"
 word_length: int = len(secret_word)
-guess: str = input(f"what is your {word_length} letter guess")
+guess: str = input(f"what is your {word_length} letter guess ")
 
 white_box: str = "\U00002B1C"
 green_box: str = "\U0001F7E9"
 yellow_box: str = "\U0001F7E8"
 
-word_index: int = 0
+word_storage: int = 0
 emoji: str = ("")
-alternate_index: int = 0
+alternate_storage: int = 0
 variable: bool = False
 
 while len(guess) != word_length:
-    print(f"That was not {word_length} letter! Try again: ")
+    print(f"That was not {word_length} letters! Try again: ")
     guess = input(f"what is your {word_length} letter guess: ")
 
 
-    while word_index < int(len(secret_word)):
-        if guess[word_index] == secret_word[word_index]:
-            emoji = emoji + green_box
-        elif guess[word_index] != secret_word[alternate_index]:
-                alternate_index = 0
-                variable = False
-                while alternate_index < word_length:
-                    if guess[word_index] == secret_word[alternate_index]:
-                        variable = True
-                    alternate_index += 1
-                if variable is True:
-                    emoji = emoji + yellow_box
-                else:
-                    emoji = emoji + white_box
-word_index = word_index + 1
-
-print (emoji)
+while word_storage < int(len(secret_word)):
+    
+    if guess[word_storage] == secret_word[word_storage]:
+        emoji = emoji + green_box
+    
+    elif guess[word_storage] != secret_word[word_storage]:
+        alternate_storage = 0
+        variable = False
+        
+        while alternate_storage < word_length:
+            
+            if guess[word_storage] == secret_word[alternate_storage]:
+                variable = True
+            alternate_storage += 1 
+        
+        if variable is True:
+            emoji = emoji + yellow_box
+        
+        else: 
+            emoji = emoji + white_box
+    word_storage = word_storage + 1
+    
+print(emoji)
 
 if guess != secret_word:
     print("Not quite. Play again soon! ")
+
 if guess == secret_word:
-    print("Woo You got it! ")
-                
+    print("Woo! You got it! ")
+
